e7aca2effa67a22abf0407f7c0597e90f31b2ebd
Captain <pengkian@163.com>
2018-02-07 21:03:06 +0800

    重启项目

--- server.js
+++ server.js
@@ -1,51 +1,18 @@
 var http = require('http');
-var url = require('url')
 var fs = require('fs');
 
-http.createServer(function(req,res){
-	var pathname = url.parse(req.url).pathname;
-	console.log("pathname:"+pathname);
-	if(pathname=='/')
-		fs.readFile('./SignIn.html',function(err,data){
-			if(err)
-				console.log('Read file error!!!');
-			else{
-				res.writeHead(200,{'Content-Type':'text/html'});
-				res.write(data);
-				res.end();
-			}
-		});
-	else if(pathname=='/picture_1.jpg'){
-		fs.readFile('./picture_1.jpg',function(err,data){
-			if(err)
-				console.log('read picture wrong!!!');
-			else{
-				res.writeHead(200,{'Content-Type':'image/jpeg'});
-				res.write(data,'binary');
-				res.end();
-			}
-		})
-	}
-	else if(pathname=='/form_submit.js'){
-		console.log('post have acceve!');
-		var dt='';
-		req.on('data',function(chunk){
-			dt+=chunk;
-		});
-		req.on('end',function(){
-			res.writeHead(200,{'Content-Type':'text/text'});
-			res.write(dt);
-			res.end();
-		});
-		// res.writeHead(200,{'Content-Type':'text/text'});
-		// res.write(JSON.stringify(req));
-		// res.end();
-	}
-	else{
-		console.log('404 Not Found !!!!');
-		res.writeHead(404,{'Content-Type':'text/text'});
-		res.write('404 Not Found Page!');
-		res.end();
-	}
-}).listen(8080);
-console.log('server running at http://127.0.0.1:8080');
+http.createServer(function(request,respond){
+	console.log('hello world');
+	fs.readFile('./login.html',function(err,data){
+		if(err)
+			console.log('读取文件错误');
+		else{
+			respond.writeHead(200,{'Content-Type':'text/html'});
+			respond.write(data);
+			respond.end();
+		}
+	})
+	
+}).listen(8888);
+
+console.log('服务器已启动');
