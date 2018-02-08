22debaf590c77cad709c898c54ae4212aa95c3c9
Captain <pengkian@163.com>
2018-02-08 21:11:57 +0800

    写入登录验证函数

--- requestHandles.js
+++ requestHandles.js
@@ -1,21 +1,54 @@
 
 var fs = require('fs');
+var qs = require('querystring');
 
 var handle = [];
 handle['/'] = index;
 handle['/login'] = login;
+handle['/login_check'] = login_check;
 
-function index(respond){
+function index(request,respond){
 	respond.writeHead(200,{'Content-Type':'text/plain'});
 	respond.write('hello world');
 	respond.end();
 }
 
-function login(respond){
+function login(request,respond){
 	respond.setHeader('Content-Type','text/html');
-	fs.createReadStream(__dirname + '/login.html').pipe(respond);
+	fs.createReadStream(__dirname +'/public'+'/login.html').pipe(respond);
 }
 
+function login_check(request,respond){
+	var postData = '';
+	request.on('data',function(chunk){
+		postData += chunk;
+	});
+	request.on('end',function(){
+		var res = qs.parse(postData);
+		respond.writeHead(200,{'Content-Type':'text/plain'});
+		respond.write(JSON.stringify(res));
+		respond.end();
+	})
+}
+
+function getImg(pathname,respond){
+	respond.setHeader('Content-Type','image/jpeg');
+	fs.createReadStream(__dirname+'/public'+pathname).pipe(respond);
+}
+function getCss(pathname,respond){
+	respond.setHeader('Content-Type','text/css');
+	fs.createReadStream(__dirname+'/public'+pathname).pipe(respond);
+}
+
+function getJs(pathname,respond){
+	respond.setHeader('Content-Type','application/x-javascript')
+	fs.createReadStream(__dirname+'/public'+pathname).pipe(respond);
+}
+
+exports.login_check = login_check;
+exports.getJs = getJs;
+exports.getCss = getCss;
+exports.getImg = getImg;
 exports.handle = handle;
 exports.index = index;
 exports.login = login;
--- server.js
+++ server.js
@@ -5,17 +5,6 @@ function start(route,requestHandles){
 	http.createServer(function(request,respond){
 		console.log('收到请求');
 		route(request,respond,requestHandles);
-
-		// console.log('hello world');
-		// fs.readFile('./login.html',function(err,data){
-		// 	if(err)
-		// 		console.log('读取文件错误');
-		// 	else{
-		// 		respond.writeHead(200,{'Content-Type':'text/html'});
-		// 		respond.write(data);
-		// 		respond.end();
-		// 	}
-		// })
 		
 	}).listen(8888);
 
