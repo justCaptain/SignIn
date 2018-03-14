c362a65d0878049122835687fbe6b4151e90ec21
Captain <pengkian@163.com>
2018-03-14 14:50:18 +0800

    完成注册的基本功能

--- routers/api.js
+++ routers/api.js
@@ -2,14 +2,14 @@
 
 var express = require("express");
 var router = express.Router();
-
+var user = require("../models/user");
 /**
  *  统一返回格式
  *  code:  
  *      0-> 成功
  *      1-> 用户名为空
  *      2-> 两次密码不一致
- * 
+ *      3-> 用户名不一致
 */
 var responseData;
 router.use(function(req,res,next){
@@ -33,19 +33,42 @@ router.post("/registerAuth",function(req,res,next){
 
     //用户名为空
     if(username == ""){
-        responseData.code = "1";
+        responseData.code = 1;
         responseData.message = "用户名不能为空"
         res.json(responseData);
         return ;
     }
+    
     //两次输入密码不一致
     if(password != repassword){
-        responseData.code = "2";
+        responseData.code = 2;
         responseData.message = "两次输入密码不一致";      
         res.json(responseData);
         return ;
     }
-    res.json(responseData);
+    //
+    user.findOne({username:username,password:password},function(err,result){
+        if(err){
+            console.log(err);
+            return ;
+        }else{
+            if(!result){
+                var newuser = new user({
+                    username: username,
+                    password: password
+                }).save()
+                    .then(function(Info){
+                        res.json(responseData);
+                        console.log(Info);
+                    });
+            }else{
+                responseData.code = 3;
+                responseData.message = "用户名已存在";
+                res.json(responseData);
+            }
+        }
+                
+    })
 }); 
 
 module.exports = router;
--- schemas/users.js
+++ schemas/users.js
@@ -2,6 +2,6 @@
 var mongoose = require("mongoose");
 
 module.exports = new mongoose.Schema({
-    user: string,
-    password: string
+    username: String,
+    password: String
 });
